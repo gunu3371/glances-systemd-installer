bc7e125bb92ec2eab70a572cbdbc66c324dba4df
gunu3371 <55932910+gunu3371@users.noreply.github.com>
2024-07-21 03:59:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 sudo apt update
 sudo apt install python3-dev -y
 sudo apt install python3-pip -y
-python3 -m pip install --upgrade pipx --break-system-packages
+sudo python3 -m pip install --upgrade pipx --break-system-packages || sudo python3 -m pip install --upgrade pipx
 sudo pipx ensurepath --global --force
 sudo pipx install 'glances[all]' --verbose
 sudo cp ./glances.service /etc/systemd/system/
+sudo systemctl stop glances.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now glances.service
