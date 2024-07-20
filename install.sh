b03a754047a1ee937728702a64f7756509e0a276
gunu3371 <55932910+gunu3371@users.noreply.github.com>
2024-07-21 04:05:28 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install python3-dev -y
 sudo apt install python3-pip -y
+sudo apt install python3-venv -y
 sudo python3 -m pip install --upgrade pipx --break-system-packages || sudo python3 -m pip install --upgrade pipx
 sudo pipx ensurepath --global --force
 sudo pipx install 'glances[all]' --verbose --force --global
